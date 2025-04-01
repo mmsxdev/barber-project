c28817bf587bdcb60e941a762727522dacacb852
Miguel Melo <hjumiguel@gmail.com>
2025-04-01 13:53:41 -0300

    fix: update server startup to use dynamic port configuration and add Prisma build scripts

--- backend/server.js
+++ backend/server.js
@@ -60,6 +60,8 @@ prisma
     console.log("Erro ao conectar no banco de dados: ", error);
   });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
