129ade07e11b29402349d96e49bc5008bb7e6e56
Miguel Melo <hjumiguel@gmail.com>
2025-04-01 11:31:31 -0300

    fix: remove logging of environment variables and database connection on server startup

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,4 @@ app.use((req, res, next) => {
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
-  console.log("NODE_ENV: ", process.env.NODE_ENV);
-  console.log("Allowed Origins: ", allowedOrigins);
-  console.log("Banco de dados conectado: ", process.env.DB_HOST);
 });
