e841264ccc5f18cccaeb585da05dceda740743ed
Miguel Melo <hjumiguel@gmail.com>
2025-04-16 18:18:49 -0300

    feat: update bcrypt dependency to bcryptjs for improved password handling; modify import in public routes

--- backend/routes/public.js
+++ backend/routes/public.js
@@ -1,6 +1,6 @@
 import express from "express";
 import rateLimit from "express-rate-limit";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
 import { checkRole } from "../Middleware/role.js";
